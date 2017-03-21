07bc2dc6a117bc4245867d9eb3b1b8d46fe47dd1
김태형_201411517 <rlaxogud94@naver.com>
2017-03-21 23:52:43 +0900

    Add files via upload

---
+++ headfirst/strategy/FlyWithWing.java
@@ -0,0 +1,7 @@
+package headfirst.strategy;
+
+public class FlyWithWing implements FlyBehavior{
+  public void fly(){
+    System.out.println("I'm flying with wings!");
+  }
+}
---
+++ headfirst/strategy/MallardDuck.java
@@ -0,0 +1,11 @@
+package headfirst.strategy;
+
+public class MallardDuck extends Duck{
+  public MallardDuck(){
+    quackBehavior=new Quack();
+    flyBehavior=new FlyWithWing();
+  }
+  public void display(){
+    System.out.println("I'm a Mallard Duck.");
+  }
+}
