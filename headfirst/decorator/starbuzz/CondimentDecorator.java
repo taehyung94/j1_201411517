2ecb0f3cf348fe71a658b8d148a8151b91e7cd3b
김태형_201411517 <rlaxogud94@naver.com>
2017-04-11 13:01:24 +0900

    Add files via upload

---
+++ headfirst/decorator/starbuzz/CondimentDecorator.java
@@ -0,0 +1,6 @@
+package headfirst.decorator.starbuzz;
+
+public abstract class CondimentDecorator extends Beverage{
+   abstract public String getDescription();
+  //public double cost();
+}
---
+++ headfirst/decorator/starbuzz/StarbuzzCoffee.java
@@ -0,0 +1,30 @@
+package headfirst.decorator.starbuzz;
+
+public class StarbuzzCoffee {
+ 
+ public static void main(String args[]) {
+  Beverage beverage = new Espresso();
+  System.out.println(beverage.getDescription() 
+    + " $" + beverage.cost());
+ 
+  Beverage beverage2 = new DarkRoast();
+  beverage2 = new Mocha(beverage2);
+  beverage2 = new Mocha(beverage2);
+  beverage2 = new Whip(beverage2);
+  System.out.println(beverage2.getDescription() 
+    + " $" + beverage2.cost());
+ 
+  Beverage beverage3 = new HouseBlend();
+  beverage3 = new Soy(beverage3);
+  beverage3 = new Mocha(beverage3);
+  beverage3 = new Whip(beverage3);
+  System.out.println(beverage3.getDescription() 
+    + " $" + beverage3.cost());
+  
+  Beverage b4=new Espresso();
+  b4=new Caramel(b4);
+  b4=new SteamedMilk(b4);
+  System.out.println(b4.getDescription()
+    + " $" + b4.cost());
+ }
+}
