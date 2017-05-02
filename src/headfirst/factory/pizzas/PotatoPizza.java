f9bdfdd65763c61ff66b7bd8e86d190f94118903
김태형_201411517 <rlaxogud94@naver.com>
2017-05-02 14:50:58 +0900

    Create PotatoPizza.java

---
+++ src/headfirst/factory/pizzas/PotatoPizza.java
@@ -0,0 +1,11 @@
+package headfirst.factory.pizzas;
+
+public class PotatoPizza extends Pizza {
+ public PotatoPizza() {
+  name = "Potato Pizza";
+  d = "Thin Crust";
+  sauce = "hot Pizza Sauce";
+  toppings.add("Fresh Mozzarella");
+  toppings.add("Parmesan");
+ }
+}
