fcd9c1858357623d249e138deb42685d511d6b44
김태형_201411517 <rlaxogud94@naver.com>
2017-05-02 15:00:29 +0900

    Update SMStyleCheesePizza.java

--- src/headfirst/factory/pizzafm/SMStyleCheesePizza.java
+++ src/headfirst/factory/pizzafm/SMStyleCheesePizza.java
@@ -1,10 +1,10 @@
 package headfirst.factory.pizzafm;
 
-public class SMStyleCheesePizza extends Pizza{
-  public SMStyleCheesePizza(){
-    name="SM Style Sauce and Cheese Pizza";
+public class SMStylePotatoPizza extends Pizza{
+  public SMStylePotatoPizza(){
+    name="SM Style Sauce and Potato Pizza";
     dough="Thick Crust";
     sauce="Hot Sauce";
-    toppings.add("Grated Reggiano Cheese");
+    toppings.add("Slice Potato");
   }
 }
