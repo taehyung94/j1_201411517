599bf453c17f8482f784d827371ea8cf0c5ddef2
김태형_201411517 <rlaxogud94@naver.com>
2017-05-09 12:58:45 +0900

    Add files via upload

---
+++ src/headfirst/singleton/chocolate/ChocolateBoiler.java
@@ -0,0 +1,48 @@
+package headfirst.singleton.chocolate;
+
+public class ChocolateBoiler{
+  private static ChocolateBoiler uniqueInstance;
+  private boolean empty;
+  private boolean boiled;
+  private static int numCalled=0;
+  private ChocolateBoiler(){
+    empty=true;
+    boiled=false;
+  }
+  public static ChocolateBoiler getInstance(){
+    if(uniqueInstance==null){
+      System.out.println("Creating..");
+      uniqueInstance=new ChocolateBoiler();
+    }
+    System.out.println("returning..");
+    System.out.println("num called.. "+numCalled++);
+    return uniqueInstance;
+  }
+  public void fill(){
+    if(isEmpty()){
+      empty=false;
+      boiled=false;
+      System.out.println("filling..");
+    }
+  }
+  public void boil(){
+    if(!isEmpty()&&!isBoiled()){
+      boiled=true;
+      System.out.println("boiling..");
+    }
+  }
+  public void drain(){
+    if(!isEmpty()&&isBoiled()){
+      empty=true;
+      System.out.println("draining..");
+    }
+  }
+  public boolean isEmpty(){
+    return empty;
+  }
+  public boolean isBoiled(){
+    return boiled;
+  }
+}
+      
+  
